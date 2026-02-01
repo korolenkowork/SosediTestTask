e80df97a73d12bf9dfd9e60ed4fcb8b70fbffe7f
Ihor <enotshow275@gmail.com>
2026-02-01 20:59:57 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sosedi API Test task
+
+## How to run application
+
+1. Run `docker compose up --build` command
+2. Open http://localhost:8000/schema/swagger/
--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ echo "Starting Migrations..."
 python manage.py migrate
 echo ====================================
 
+echo "Collecting Test Data..."
+python manage.py createtestcollections 100
+echo ====================================
+
 echo "Starting Server..."
 python manage.py runserver 0.0.0.0:8000
